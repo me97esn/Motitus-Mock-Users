d9a78019540b87bc51a4148eeb7cac06d52e82b6
Emil Stenberg <esandin@gmail.com>
2017-01-01 22:06:25 +0100

    connecting ten mock users

--- index.js
+++ index.js
@@ -1,52 +1,40 @@
 const rp = require('request-promise')
+const socketIO = require('socket.io-client')
+// Synchronous is fine
+for (var i = 0; i < 10; i++) {
+  let token, userId
 
-var options = {
-    method: 'POST',
-    uri: 'http://localhost:4000/api/user',
-    body: {},
-    json: true // Automatically stringifies the body to JSON
+  const socket = socketIO('http://localhost:4000')
+  socket.on('connect', ()=>console.log('connected'))
+  const location = {
+      coords: {
+          latitude: 59.28832,
+          accuracy: 5,
+          longitude: 18.11787,
+          altitude: 0
+      }
+  }
+
+
+  function authenticate(){
+    socket.emit('authenticate', {
+          token,
+          location})
+  }
+
+  rp({
+      method: 'POST',
+      uri: 'http://localhost:4000/api/user',
+      body: {},
+      json: true
+  })
+      .then(body => {
+        token = body.token
+        userId = body.userId
+        return
+      })
+      .then(authenticate)
+      .catch(function (err) {
+        console.error(err)
+      })
 }
-
-const socket = require('socket.io-client')('http://localhost:4000');
-socket.on('connect', ()=>console.log('connected'))
-const location = {
-    coords: {
-        latitude: 59.28832,
-        accuracy: 5,
-        longitude: 18.11787,
-        altitude: 0
-    }
-}
-
-function authenticate(){
-  socket.emit('authenticate', {
-        token,
-        location})
-}
-
-let token, userId
-rp(options)
-    .then(body => {
-      token = body.token
-      userId = body.userId
-      return
-    })
-    .then(authenticate)
-    .catch(function (err) {
-        // POST failed...
-    });
-
-
-/*
-"authenticate",
-        {
-            "location": {
-                "coords": {
-                    "latitude": 59.28832,
-                    "accuracy": 5,
-                    "longitude": 18.11787,
-                    "altitude": 0
-                }
-            },
-            "token": "eyJhbGciOiJIUzI1NiJ9.ZjgzNTE5NzItMmQ3Yi00YzYwLTk1ZjAtMjY5ZTA4OTcwYzZi.m-gFhljdZ6F7UQg9uHnpK8Abd6icavNCuaa3j7wPhps"
-        }*/
