dc0a7afcf5d48a3e40249708a8b245dcb5e673e8
Emil Stenberg <esandin@gmail.com>
2017-01-01 22:29:35 +0100

    stepping on regular intervals

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const rp = require('request-promise')
 const socketIO = require('socket.io-client')
+
+const numberOfUsers = 10
+const latitude = 59.28832
+const longitude = 18.11787
+
 // Synchronous is fine
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < numberOfUsers; i++) {
   let token, userId
 
   const socket = socketIO('http://localhost:4000')
   socket.on('connect', ()=>console.log('connected'))
   const location = {
       coords: {
-          latitude: 59.28832,
+          latitude:latitude + (Math.random()-0.5) / 1000,
           accuracy: 5,
-          longitude: 18.11787,
+          longitude:longitude + (Math.random()-0.5) / 1000,
           altitude: 0
       }
   }
@@ -20,6 +25,14 @@ for (var i = 0; i < 10; i++) {
     socket.emit('authenticate', {
           token,
           location})
+  }
+
+  function step(){
+    location.coords.latitude += 0.00001
+    location.coords.longitude += 0.00001
+    socket.emit('location', {
+          location})
+
   }
 
   rp({
@@ -34,6 +47,9 @@ for (var i = 0; i < 10; i++) {
         return
       })
       .then(authenticate)
+      .then(()=> setInterval(()=>{
+        step()
+      },1000))
       .catch(function (err) {
         console.error(err)
       })
