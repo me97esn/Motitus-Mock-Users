033830a63fb8745c207fb8beec24661632b80b17
Emil Stenberg <emilsten@kth.se>
2017-01-11 10:01:00 +0100

    use tcp socket instead of websocket

---
+++ tcpSocketClient.js
@@ -0,0 +1,19 @@
+// var WebSocket = require('ws')
+// var ws = new WebSocket('ws://130.237.31.26:1755')
+
+var net = require('net')
+
+var client = new net.Socket()
+client.connect(1755, '130.237.31.26', function () {
+  console.log('Connected')
+  client.write('Hello, server! Love, Client.')
+})
+
+client.on('data', function (data) {
+  console.log('Received: ' + data)
+  client.destroy() // kill client after server's response
+})
+
+client.on('close', function () {
+  console.log('Connection closed')
+})
